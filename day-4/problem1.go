823fcb0593630eb139e59945c97e9b1d6ebdb2be
Altaha <altahabinsyafriadi@gmail.com>
2021-10-25 13:48:44 +0700

    senin v1

--- day-4/problem1.go
+++ day-4/problem1.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"math"
@@ -16,25 +17,25 @@ func Compare(a, b string) string {
 		shortStr = b
 	}
 
-	var comStr, theSame string
+	var initStr, same string
 	var temp int
 
 	for i := range shortStr {
 		for j := range longStr {
 			temp = 0
-			theSame = ""
+			same = ""
 
 			for int(i+temp) < shortLen && int(j+temp) < maxLen && shortStr[i+temp] == longStr[j+temp] {
-				theSame += string(longStr[j+temp])
+				same += string(longStr[j+temp])
 				temp += 1
 			}
 
-			if len(theSame) > len(comStr) {
-				comStr = theSame
+			if len(same) > len(initStr) {
+				initStr = same
 			}
 		}
 	}
-	return comStr
+	return initStr
 }
 
 func main() {
@@ -44,5 +45,4 @@ func main() {
 	fmt.Println(Compare("KUPU-KUPU", "KUPU")) // KUPU
 	fmt.Println(Compare("ILALANG", "ILA")) // ILA
 	fmt.Println(Compare("SEPATU", "EPA")) // EPA
-	fmt.Println(Compare("ABCD", "KFAN")) //
 }
