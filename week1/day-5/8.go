0d6ea5d99381304468b485a77d9bd757b8565969
Altaha <altahabinsyafriadi@gmail.com>
2021-10-27 16:49:22 +0700

    rabu

--- week1/day-5/8.go
+++ week1/day-5/8.go
@@ -14,8 +14,6 @@ type appear struct {
 
 func MostAppearItem(items []string) []pair {
 
-	// your code here
-
 	itemToCount := make(map[string]int)
 
 	for _, v := range items {
@@ -54,15 +52,12 @@ func MostAppearItem(items []string) []pair {
 func main() {
 
 	fmt.Println(MostAppearItem([]string{"js", "js", "golang", "ruby", "ruby", "js", "js"}))
-
 	// golang->1 ruby->2 js->4
 
 	fmt.Println(MostAppearItem([]string{"A", "B", "B", "C", "A", "A", "B", "A", "D", "D"}))
-
 	// C->1 D->2 B->3 A->4
 
 	fmt.Println(MostAppearItem([]string{"football", "basketball", "tenis"}))
-
 	// football->1 basketball->1 tenis->1
 
 }
