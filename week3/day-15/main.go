0ca1cf59375d22c01592fbd16f24205d804d86ab
Altaha <altahabinsyafriadi@gmail.com>
2021-11-15 13:09:29 +0700

    monday

--- week3/day-15/main.go
+++ week3/day-15/main.go
@@ -7,9 +7,6 @@ import (
 
 func main() {
 	config.InitDB()
-	// http.HandleFunc("/articles", ReadArcticles)
-	// fmt.Println("server is starting")
-	// http.ListenAndServe(":1234", nil)
 	e:=route.New()
 	e.Start(":8000")
 }
