9b460a84c4302fa1981810b0bcecadafe1966674
Altaha <altahabinsyafriadi@gmail.com>
2021-11-16 10:01:23 +0700

    selasa v1

--- week3/day-15/controller/user_controller.go
+++ week3/day-15/controller/user_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 	"project/config"
+	m "project/middleware"
 	"project/model"
 
 	"github.com/labstack/echo"
@@ -39,4 +40,35 @@ func CreateUserController(c echo.Context) error {
 		"user":    user,
 	})
 
+}
+
+func LoginUserController(c echo.Context)error{
+	user := model.Users{}
+	c.Bind(&user)
+
+	err := config.DB.Where("email=? AND password=?",user.Email,user.Password).First(&user).Error
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "fail login",
+			"error":err.Error(),
+		})
+	}
+
+	token,err:=m.CreateToken(user.ID,user.Name)
+	
+	if(err!=nil){
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "fail login",
+			"error":err.Error(),
+		})
+	}
+
+	
+	userResponse:=model.UsersResponse{user.ID,user.Name,user.Email,token}
+	
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create user",
+		"user":    userResponse,
+	})
 }
--- week3/day-15/route/route.go
+++ week3/day-15/route/route.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+	"project/constants"
 	c "project/controller"
 	m "project/middleware"
 
@@ -13,9 +14,16 @@ func New() *echo.Echo{
 	e.GET("/users", c.GetUserController)
 	m.LogMiddleware(e)
 	e.POST("/users",c.CreateUserController)
+	e.POST("/login",c.LoginUserController)
 	
 	eAuth:=e.Group("/auth")
 	eAuth.Use(em.BasicAuth(m.BasicAuthDB))
 	eAuth.GET("/users",c.GetUserController)
+
+	eJwt:=e.Group("/jwt")
+	eJwt.Use(em.JWT([]byte(constants.SECRET_JWT)))
+	eJwt.GET("/users",c.GetUserController)
+	
+	
 	return e
 }
